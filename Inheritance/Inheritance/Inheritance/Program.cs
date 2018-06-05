7b0761b0b7d9333528f3a51c0eb7b9da08724860
fairo <fairoliur@gmail.com>
2018-06-05 14:55:34 +0800

    Inheritance

---
+++ Inheritance/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            One one = new One();
+        }
+    }
+
+    class Animal {
+        public int Age { get; set; }
+        public virtual void Bite()
+        {
+            Console.WriteLine("Animal Bite");
+        }
+
+        public virtual void GetAge()
+        {
+            Console.WriteLine (Age);
+        }
+
+        public void BiteMan()
+        {
+            Console.WriteLine("Animal Bite Man");
+        }
+    }
+
+    class Dog:Animal
+    {
+        public override void Bite()
+        {
+            Console.Write("Dog Bite");
+        }
+
+        public new void BiteMan()
+        {
+            Console.WriteLine("Dog Bite Man");
+        }
+    }
+
+    class Human { }
+    class Man:Human { }
+    class One : Man { }
+}
