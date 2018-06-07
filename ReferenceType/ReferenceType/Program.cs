90564c0a3dba43df1e7e61f877b7a9d61f0eadd4
fairo-lr <fairoliur@gmail.com>
2018-06-07 22:53:20 +0800

    ReferenceType

---
+++ ReferenceType/ReferenceType/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.GetName2();//可以直接调用
+            p.GetName();
+        }
+    }
+
+    class Person : Man, ISuper
+    {
+        public int GetSuper() { return 0; } //interface的方法必须实现
+        public override void GetName() {
+            //必须继承, 否则报错
+        }
+    }
+    abstract class Man
+    {
+        public string name;
+
+        public string GetName2()
+        {
+            return name;
+        }
+
+        public abstract void GetName();
+    }
+    interface ISuper
+    {
+        int GetSuper();
+    }
+
+}
