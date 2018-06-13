66ccf159eeda7322fa0414213fabbb4f344c9fcd
fairo <fairoliur@gmail.com>
2018-06-13 16:24:53 +0800

    regex

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Regex
+{
+    using System.Text.RegularExpressions;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 使用C#自带的正则表达式
+            var s1 = "123";
+            var param = "123";
+            Console.WriteLine(Regex.IsMatch(s1, param));
+
+            Console.ReadLine();
+
+        }
+    }
+}
